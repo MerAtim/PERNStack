8a7fa9fd7a34c5d2df5c76bec524113c9086bddb
Meratim <guerra.mercedes@hotmail.com>
2025-08-14 03:36:35 -0300

    Agrego Clase 1 de JavaScript: E-commerce

---
+++ Programacion IV/JavaScript/E-commerce/client/js/products.js
@@ -0,0 +1,65 @@
+const productos = [
+    {
+        id: 1,
+        productName:"Ciruela por kilo",
+        price: 1500,
+        quantity: 10,
+        image: "../media/ciruelas.jpg",
+    },
+    {
+        id: 2,
+        productName:"Durazno por kilo",
+        price: 2500,
+        quantity: 20,
+        image: "../media/durazno.jpg",
+    },
+    {
+        id: 3,
+        productName:"Frutos secos por kilo",
+        price: 10500,
+        quantity: 100,
+        image: "../media/frutos secos.jpg"
+    },
+    {
+        id: 4,
+        productName:"Huevos por docena",
+        price: 2000,
+        quantity: 100,
+        image: "../media/huevos.jpg"
+    },
+    {
+        id: 5,
+        productName:"Kinotos por kilo",
+        price: 9500,
+        quantity: 19,
+        image: "../media/kinotos.jpg"
+    },
+    {
+        id: 6,
+        productName:"Limon por kilo",
+        price: 1900,
+        quantity: 50,
+        image: "../media/limon.jpg"
+    },
+    {
+        id: 7,
+        productName:"Naranja por kilo",
+        price: 900,
+        quantity: 100,
+        image: "../media/naranjas.jpg"
+    },
+    {
+        id: 8,
+        productName:"Nueces por 100 gramos",
+        price: 2000,
+        quantity: 500,
+        image: "../media/nueces.jpg"
+    },
+    {
+        id: 9,
+        productName:"Pomelo por kilo",
+        price: 1900,
+        quantity: 150,
+        image: "../media/pomelos.jpg"
+    }
+];
