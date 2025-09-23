6d20d3de6bf57f7ca07a546933fe0eac0ed73d3c
MerAtim <guerra.mercedes@hotmail.com>
2025-09-23 01:05:24 -0300

    Agrego carpeta Juego Tecnicatura

---
+++ Programacion IV/Juego_tecnicatura/constantes.py
@@ -0,0 +1,12 @@
+import os
+
+# Dimensiones de pantalla
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Especificando el color de la pantalla
+COLOR_LASER = (0, 0, 255) # Color azul para el láser
+
+# Ruta a la carpeta Assets
+ASSETS_PATH = os.path.join(os.path.dirname(__file__), 'assets')
---
+++ Programacion IV/Juego_tecnicatura/main.py
@@ -0,0 +1,7 @@
+import pygame
+import sys
+import random
+import os
+
+from personaje import Personaje, Enemigos, Explosion
+from constantes import SCREEN_WIDTH, SCREEN_HEIGHT, ASSETS_PATH
