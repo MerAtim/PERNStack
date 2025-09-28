6b9c073a4ae7939d6d6c5e1129216467591f6b29
MerAtim <guerra.mercedes@hotmail.com>
2025-09-27 23:04:33 -0300

    Agrego paquetes, clases y metodos a Tienda Libros

--- Programacion IV/Java/Tienda Libros/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Programacion IV/Java/Tienda Libros/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -8,6 +8,7 @@ public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaLibrosApplication.class, args);
-	}
+        System.out.println("Tienda de Libros Application Started");
 
+	}
 }
---
+++ Programacion IV/Java/Tienda Libros/tienda_libros/src/main/java/utn/tienda_libros/servicio/LibroServicio.java
@@ -0,0 +1,35 @@
+package utn.tienda_libros.servicio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import utn.tienda_libros.modelo.Libro;
+import utn.tienda_libros.repository.LibroRepositorio;
+
+import java.util.List;
+
+@Service
+public class LibroServicio implements ILibroServicio{
+
+    @Autowired // inyecta el repositorio a la clase servicio
+    private LibroRepositorio libroRepositorio;
+
+    @Override
+    public List<Libro> listarLibros() {
+        return libroRepositorio.findAll();
+    }
+
+    @Override
+    public Libro buscarLibroPorId(Integer idLibro) {
+        return libroRepositorio.findById(idLibro).orElse(null);
+    }
+
+    @Override
+    public void guardarLibro(Libro libro) {
+        libroRepositorio.save(libro);
+    }
+
+    @Override
+    public void eliminarLibro(Libro libro) {
+        libroRepositorio.delete(libro);
+    }
+}
